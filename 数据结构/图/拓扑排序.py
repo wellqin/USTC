8b883f5f9a6a694c586480d37853ca494db320d4
wellqin <1050346754@qq.com>
2019-09-10 20:29:12 +0800

    apply堵塞式多进程-线程池通信

--- 数据结构/图/拓扑排序.py
+++ 数据结构/图/拓扑排序.py
@@ -13,11 +13,11 @@ def topsort(G):
     in_degrees = dict((u, 0) for u in G)
     for u in G:
         for v in  G[u]:
-            in_degrees[v] += 1          # 每一个节点的入度
+            in_degrees[v] += 1                 # 每一个节点的入度
     Q = [u for u in G if in_degrees[u] == 0]          # 入度为 0 的节点   # 遍历in_degrees也不影响
     S = []
     while Q:
-        u = Q.pop()                  # 默认从最后一个移除
+        u = Q.pop()                    # 默认从最后一个移除
         S.append(u)
         for v in G[u]:
             in_degrees[v] -= 1           # 并移除其指向
@@ -26,7 +26,7 @@ def topsort(G):
     if len(S) == len(in_degrees):  # 如果循环结束后存在非0入度的顶点说明图中有环，不存在拓扑排序
         return S
     else:
-        return -1
+        return S
 
 G = {
     'a':'bf',
