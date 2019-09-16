ce3e9c9b16141855dc443e26ed2c16cf0a24e895
wellqin <1050346754@qq.com>
2019-09-16 20:01:22 +0800

    线程顺序执行与线程并发执行

--- 进程线程/多线程/5-线程的执⾏顺序.py
+++ 进程线程/多线程/5-线程的执⾏顺序.py
@@ -25,6 +25,17 @@ def test():
     for i in range(5):
         t = MyThread()
         t.start()
+        t.join()  # join使得线程按照顺序执行
+
+# def test():
+#     MyThreads = []
+#     for i in range(5):
+#         t = MyThread()
+#         t.start()
+#         MyThreads.append(t)
+#     for i in MyThreads:
+#         i.join()  # join加在这个位置使得线程并发执行
+
 
 
 if __name__ == '__main__':
@@ -55,4 +66,25 @@ I'm Thread-5 @ 2
 当执⾏到sleep语句时，线程将被阻塞（Blocked），到sleep结束后，线程进⼊就绪（Runnable）状态，等待调度。
 ⽽线程调度将⾃⾏选择⼀个线程执⾏。上⾯的代码中只能保证每个线程都运⾏完整个run函数，但是线程的启动顺序、
 run函数中每次循环的执⾏顺序都不能确定。
+"""
+
+"""
+t.join()使得线程按照顺序执行
+
+
+I'm Thread-1 @ 0
+I'm Thread-1 @ 1
+I'm Thread-1 @ 2
+I'm Thread-2 @ 0
+I'm Thread-2 @ 1
+I'm Thread-2 @ 2
+I'm Thread-3 @ 0
+I'm Thread-3 @ 1
+I'm Thread-3 @ 2
+I'm Thread-4 @ 0
+I'm Thread-4 @ 1
+I'm Thread-4 @ 2
+I'm Thread-5 @ 0
+I'm Thread-5 @ 1
+I'm Thread-5 @ 2
 """
