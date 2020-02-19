517510a4edf57580c104f31e57d55ee15833addf
wellqin <1050346754@qq.com>
2020-02-19 20:13:16 +0800

    [76]最小覆盖子串

--- leetcode/editor/cn/[76]最小覆盖子串.py
+++ leetcode/editor/cn/[76]最小覆盖子串.py
@@ -1,9 +1,9 @@
-#给定一个字符串 S 和一个字符串 T，请在 S 中找出包含 T 所有字母的最小子串。 
+# 给定一个字符串 S 和一个字符串 T，请在 S 中找出包含 T 所有字母的最小子串。
 #
 # 示例： 
 #
 # 输入: S = "ADOBECODEBANC", T = "ABC"
-#输出: "BANC" 
+# 输出: "BANC"
 #
 # 说明： 
 #
@@ -12,54 +12,78 @@
 # 如果 S 中存在这样的子串，我们保证它是唯一的答案。 
 # 
 #
+
+
+"""
+滑动窗口算法的思路是这样：
+1、我们在字符串 S 中使用双指针中的左右指针技巧，初始化 left = right = 0，把索引闭区间 [left, right] 称为一个「窗口」。
+2、我们先不断地增加 right 指针扩大窗口 [left, right]，直到窗口中的字符串符合要求（包含了 T 中的所有字符）。
+3、此时，我们停止增加 right，转而不断增加 left 指针缩小窗口 [left, right]，直到窗口中的字符串不再符合要求
+  （不包含 T 中的所有字符了）。同时，每次增加 left，我们都要更新一轮结果。
+4、重复第 2 和第 3 步，直到 right 到达字符串 S 的尽头。
+
+这个思路其实也不难，第 2 步相当于在寻找一个「可行解」，然后第 3 步在优化这个「可行解」，最终找到最优解。
+左右指针轮流前进，窗口大小增增减减，窗口不断向右滑动。
+
+一个哈希表 needs 记录字符串 t 中包含的字符及出现次数，用另一个哈希表 window 记录当前「窗口」中包含的字符及出现的次数，
+如果 window 包含所有 needs 中的键，且这些键对应的值都大于等于 needs 中的值，那么就可以知道当前「窗口」符合要求了，
+可以开始移动 left 指针了
+
 """
 
-首先搞几个corner case
-开始步入正题，我们脑子里面必须要有一个window的概念，只要这个window的end小于len(s)，就一直搞下去
 
-记录一下t中字符和其频率，然后随着end的变大不停更新这个maps，即出现一个字符，
-如果该字符在maps里面，我们就对应的自减一下这个字符在maps中的频率
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        N = len(s)
+        left = 0
+        res = ''
+        min_val = N
+        match = 0
+        needs = {}  # 最小子串需要哪些元素，即将T中元素进行统计
+        windows = {}  # 维持needs中元素的窗口
 
-用一个counter记录这个maps里面频率不为0的字符个数，即key个数，当counter为0的时候我们就知道t中所以的字符都在我们的window里面了
-这个时候，我们就可以开始自增begin了，然后只要仍然可以保持counter为0（即所有字符都还在window里面），我们就一直移动begin以减小window的size
-当最后while end < len(s)循环出来的时候，我们判断一下我们的window的length是不是变化了（最开始设置了inf最大值），
-如果变化了说明我们找到了符合要求的window，否则没有
-"""
+        for i in t:
+            needs[i] = needs.get(i, 0) + 1
 
-import collections
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        if len(t) > len(s):
-            return ''
-        if s == '' or t == '':
-            return ''
-        maps = collections.Counter(t)
-        counter = len(maps.keys())
-        begin, end, head, length = 0, 0, 0, float('inf')
-        while end < len(s):
-            if s[end] in maps:
-                maps[s[end]] -= 1
-                if maps[s[end]] == 0:
-                    counter -= 1
-            end += 1
-            while counter == 0:
-                if s[begin] in maps:
-                    maps[s[begin]] += 1
-                    if maps[s[begin]] > 0:
-                        counter += 1
-                    if end - begin < length:
-                        length = end - begin
-                        head = begin
-                begin += 1
-        if length == float('inf'):
-            return ''
-        return s[head:head+length]
+        match_N = len(needs)
 
-S = "ADOBECODEBANC"
+        for right in range(N):
+            if s[right] in needs:
+                windows[s[right]] = windows.get(s[right], 0) + 1
+                if windows[s[right]] == needs[s[right]]:
+                    match += 1
+                while match == match_N:  # 当windows中包含了needs中的所有元素，windows可能有重复的
+                    # 此while循环，直到窗口中的字符串不再符合要求，left 不再继续移动
+                    this_time_val = right - left
+                    if this_time_val < min_val:  # 每次找到比min_val更小的this_time_val区间时，更新区间
+                        res = s[left:right + 1]
+                        min_val = this_time_val
+
+                    if s[left] in needs:
+                        windows[s[left]] = windows[s[left]] - 1  # 去除边界的重复值
+                        if windows[s[left]] < needs[s[left]]:  # 如果去重复值后，窗口中的字符串不再符合要求，则match自减，while结束
+                            match -= 1
+                    left += 1
+        return res
+
+    # def minWindowSelf(self, s: str, t: str) -> str:
+    #     if not s or not t:
+    #         return ""
+    #     left = right = 0
+    #     windows = {}
+    #     needs = {}
+    #     n = len(s)
+    #     m = len(t)
+    #     for i in range(m):
+    #         if i not in needs:
+    #             needs[t[i]] = i
+    #     for i in range(n):
+    #         if s[i] in needs:
+    #             windows[t[i]] = 1
+    #             right += 1
+    #         if windows
+
+
+S = "ABBECODEBANC"
 T = "ABC"
 print(Solution().minWindow(S, T))
