929344a4f32fe4c97ed172d9d095fd87690813f0
wellqin <1050346754@qq.com>
2020-05-12 17:57:22 +0800

    update

--- debug.py
+++ debug.py
@@ -1,1 +1,10 @@
 # Test File
+import requests
+
+print("docker remote")
+
+
+# https://api.lovelive.tools/api/SweetNothings
+# r = requests.get('https://api.lovelive.tools/api/SweetNothings')
+# print(r.content.decode("utf-8"))
+print("的数据\t数据")
